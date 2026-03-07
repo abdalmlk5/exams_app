f367ed0a0b97000c77808c65e70a96ccbb9ff1c9
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-03-07 20:40:45 +0200

    chore(deps): Update dependencies and build runner

--- lib/config/di/di.config.dart
+++ lib/config/di/di.config.dart
@@ -13,7 +13,21 @@ import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
-import '../dio/dio_module.dart' as _i1047;
+import '../../features/authentication/api/api_client/auth_api_client.dart'
+    as _i326;
+import '../../features/authentication/api/datasources/auth_remote_data_source_impl.dart'
+    as _i356;
+import '../../features/authentication/data/datasources/auth_remote_data_source_contract.dart'
+    as _i793;
+import '../../features/authentication/data/repositories/auth_repo_impl.dart'
+    as _i836;
+import '../../features/authentication/domain/repositories/auth_repo_contract.dart'
+    as _i880;
+import '../../features/authentication/domain/usecases/login_usecase.dart'
+    as _i995;
+import '../../features/authentication/domain/usecases/register_usecase.dart'
+    as _i257;
+import '../dio/dio_module.dart' as _i977;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -24,8 +38,26 @@ extension GetItInjectableX on _i174.GetIt {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     final dioModule = _$DioModule();
     gh.lazySingleton<_i361.Dio>(() => dioModule.dio);
+    gh.factory<_i326.AuthApiClient>(() => _i326.AuthApiClient(gh<_i361.Dio>()));
+    gh.factory<_i793.AuthRemoteDataSourceContract>(
+      () => _i356.AuthRemoteDataSourceImpl(
+        authApiClient: gh<_i326.AuthApiClient>(),
+      ),
+    );
+    gh.factory<_i880.AuthRepoContract>(
+      () => _i836.AuthRepoImpl(
+        authRemoteDataSourceContract: gh<_i793.AuthRemoteDataSourceContract>(),
+      ),
+    );
+    gh.factory<_i995.LoginUsecase>(
+      () => _i995.LoginUsecase(authRepoContract: gh<_i880.AuthRepoContract>()),
+    );
+    gh.factory<_i257.RegisterUsecase>(
+      () =>
+          _i257.RegisterUsecase(authRepoContract: gh<_i880.AuthRepoContract>()),
+    );
     return this;
   }
 }
 
-class _$DioModule extends _i1047.DioModule {}
+class _$DioModule extends _i977.DioModule {}
