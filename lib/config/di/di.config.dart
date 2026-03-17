92f0f92ba2dda29f12647d421fe6962084d8ecbe
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-03-17 20:54:20 +0200

    feat(auth): integrate remember me usecase into auth cubit and DI

--- lib/config/di/di.config.dart
+++ lib/config/di/di.config.dart
@@ -32,6 +32,8 @@ import '../../features/authentication/domain/usecases/forget_password_use_case.d
     as _i601;
 import '../../features/authentication/domain/usecases/get_user_data_usecase.dart'
     as _i932;
+import '../../features/authentication/domain/usecases/is_remembered_usecase.dart'
+    as _i330;
 import '../../features/authentication/domain/usecases/login_usecase.dart'
     as _i995;
 import '../../features/authentication/domain/usecases/logout_usecase.dart'
@@ -45,7 +47,7 @@ import '../../features/authentication/domain/usecases/verify_code_use_case.dart'
 import '../../features/authentication/presentation/auth/cubit/auth_cubit.dart'
     as _i788;
 import '../../features/authentication/presentation/forget_password/cubit/forget_password_cubit.dart'
-    as _i622;
+    as _i34;
 import '../../features/authentication/presentation/login/cubit/login_cubit.dart'
     as _i339;
 import '../../features/authentication/presentation/register/cubit/register_cubit.dart'
@@ -81,19 +83,14 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i802.AuthRepo>(
       () => _i836.AuthRepoImpl(
         authRemoteDataSourceContract: gh<_i793.AuthRemoteDataSourceContract>(),
+        authLocalDataSourceContract: gh<_i741.AuthLocalDataSourceContract>(),
       ),
-    );
-    gh.factory<_i601.ForgetPasswordUseCase>(
-      () => _i601.ForgetPasswordUseCase(gh<_i802.AuthRepo>()),
-    );
-    gh.factory<_i800.ResetPasswordUseCase>(
-      () => _i800.ResetPasswordUseCase(gh<_i802.AuthRepo>()),
-    );
-    gh.factory<_i121.VerifyCodeUseCase>(
-      () => _i121.VerifyCodeUseCase(gh<_i802.AuthRepo>()),
     );
     gh.factory<_i932.GetUserDataUsecase>(
       () => _i932.GetUserDataUsecase(authRepoContract: gh<_i802.AuthRepo>()),
+    );
+    gh.factory<_i330.IsRememberedUsecase>(
+      () => _i330.IsRememberedUsecase(authRepoContract: gh<_i802.AuthRepo>()),
     );
     gh.factory<_i995.LoginUsecase>(
       () => _i995.LoginUsecase(authRepoContract: gh<_i802.AuthRepo>()),
@@ -107,21 +104,31 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i633.RegisterCubit>(
       () => _i633.RegisterCubit(gh<_i257.RegisterUsecase>()),
     );
-    gh.factory<_i622.ForgetPasswordCubit>(
-      () => _i622.ForgetPasswordCubit(
-        gh<_i601.ForgetPasswordUseCase>(),
-        gh<_i121.VerifyCodeUseCase>(),
-        gh<_i800.ResetPasswordUseCase>(),
-      ),
-    );
     gh.factory<_i788.AuthCubit>(
       () => _i788.AuthCubit(
         gh<_i1067.LogoutUsecase>(),
         gh<_i932.GetUserDataUsecase>(),
+        gh<_i330.IsRememberedUsecase>(),
       ),
+    );
+    gh.factory<_i601.ForgetPasswordUseCase>(
+      () => _i601.ForgetPasswordUseCase(gh<_i802.AuthRepo>()),
+    );
+    gh.factory<_i800.ResetPasswordUseCase>(
+      () => _i800.ResetPasswordUseCase(gh<_i802.AuthRepo>()),
+    );
+    gh.factory<_i121.VerifyCodeUseCase>(
+      () => _i121.VerifyCodeUseCase(gh<_i802.AuthRepo>()),
     );
     gh.factory<_i339.LoginCubit>(
       () => _i339.LoginCubit(gh<_i995.LoginUsecase>()),
+    );
+    gh.factory<_i34.ForgetPasswordCubit>(
+      () => _i34.ForgetPasswordCubit(
+        gh<_i601.ForgetPasswordUseCase>(),
+        gh<_i121.VerifyCodeUseCase>(),
+        gh<_i800.ResetPasswordUseCase>(),
+      ),
     );
     return this;
   }
