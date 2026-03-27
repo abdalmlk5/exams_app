468d63e4067dbad214c73cab81215dcafa6b3522
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-03-27 23:29:04 +0200

    refactor(auth): rename auth response model files for clarity

--- lib/features/authentication/data/response/auth_response_model.g.dart
+++ lib/features/authentication/data/response/auth_response_model.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'auth_response.dart';
+part of 'auth_response_model.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
