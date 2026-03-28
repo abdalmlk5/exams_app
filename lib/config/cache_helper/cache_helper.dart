e085c8df511970af22011a12e940ddb02af44217
Abdelmalek <m7244985@gmail.com>
2026-03-28 07:33:08 +0200

    refactor: make CacheHelper methods static and remove injectable dependency

--- lib/config/cache_helper/cache_helper.dart
+++ lib/config/cache_helper/cache_helper.dart
@@ -1,50 +1,46 @@
 import 'package:exams_app/core/utils/app_keys.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:injectable/injectable.dart';
 
-@lazySingleton
 class CacheHelper {
-  final FlutterSecureStorage _secureStorage;
+  static const FlutterSecureStorage _secureStorage = FlutterSecureStorage();
 
-  CacheHelper(this._secureStorage);
-
-  Future<void> saveToken(String token) async {
+  static Future<void> saveToken(String token) async {
     await _secureStorage.write(key: AppKeys.tokenKey, value: token);
   }
 
-  Future<String?> getToken() async {
+  static Future<String?> getToken() async {
     return await _secureStorage.read(key: AppKeys.tokenKey);
   }
 
-  Future<void> deleteToken() async {
+  static Future<void> deleteToken() async {
     await _secureStorage.delete(key: AppKeys.tokenKey);
   }
 
-  Future<void> saveRememberMe(bool value) async {
+  static Future<void> saveRememberMe(bool value) async {
     await _secureStorage.write(
       key: AppKeys.rememberMeKey,
       value: value.toString(),
     );
   }
 
-  Future<bool> getRememberMe() async {
+  static Future<bool> getRememberMe() async {
     String? value = await _secureStorage.read(key: AppKeys.rememberMeKey);
     return value == 'true';
   }
 
-  Future<void> writeData({required String key, required String value}) async {
+  static Future<void> writeData({required String key, required String value}) async {
     await _secureStorage.write(key: key, value: value);
   }
 
-  Future<String?> readData({required String key}) async {
+  static Future<String?> readData({required String key}) async {
     return await _secureStorage.read(key: key);
   }
 
-  Future<void> deleteData({required String key}) async {
+  static Future<void> deleteData({required String key}) async {
     await _secureStorage.delete(key: key);
   }
 
-  Future<void> clearAllData() async {
+  static Future<void> clearAllData() async {
     await _secureStorage.deleteAll();
   }
 }
