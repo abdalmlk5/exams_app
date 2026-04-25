41e714e6ff5d769b48149c8361a15bef2ebc94ae
Abdelmalek <m7244985@gmail.com>
2026-04-25 21:37:53 +0200

    refactor(auth): convert ResetPasswordPage to StatefulWidget and implement controller disposal

--- lib/features/authentication/presentation/forget_password/pages/reset_password_page.dart
+++ lib/features/authentication/presentation/forget_password/pages/reset_password_page.dart
@@ -1,25 +1,40 @@
 import 'package:exams_app/core/utils/app_strings.dart';
+import 'package:exams_app/core/widgets/custom_snack_bar.dart';
 import 'package:exams_app/core/widgets/custom_text_field.dart';
 import 'package:exams_app/features/authentication/presentation/auth/widgets/widgets/auth_app_bar.dart';
-import 'package:exams_app/core/widgets/custom_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
+import '../cubit/forget_password_cubit.dart';
 import '../cubit/forget_password_event.dart';
 import '../cubit/forget_password_state.dart';
-import '../cubit/forget_password_cubit.dart';
 
-class ResetPasswordPage extends StatelessWidget {
+class ResetPasswordPage extends StatefulWidget {
   const ResetPasswordPage({super.key});
+
+  @override
+  State<ResetPasswordPage> createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  final passwordController = TextEditingController();
+
+  final confirmPasswordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final passwordController = TextEditingController();
-    final confirmPasswordController = TextEditingController();
-    final formKey = GlobalKey<FormState>();
 
     return BlocListener<ForgetPasswordCubit, ForgetPasswordState>(
       listenWhen: (prev, curr) =>
@@ -28,7 +43,10 @@ class ResetPasswordPage extends StatelessWidget {
               curr.forgetPasswordState.errorMessage,
       listener: (context, state) {
         if (state.forgetPasswordState.errorMessage != null) {
-          CustomSnackBar.error(context, state.forgetPasswordState.errorMessage!);
+          CustomSnackBar.error(
+            context,
+            state.forgetPasswordState.errorMessage!,
+          );
           return;
         }
         if (state.forgetPasswordState.data != null) {
@@ -50,7 +68,10 @@ class ResetPasswordPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SizedBox(height: 40.h),
-                  Text(AppStrings.resetPassword, style: theme.textTheme.titleLarge),
+                  Text(
+                    AppStrings.resetPassword,
+                    style: theme.textTheme.titleLarge,
+                  ),
                   SizedBox(height: 16.h),
                   Text(
                     AppStrings.resetPasswordSubtitle,
@@ -80,11 +101,11 @@ class ResetPasswordPage extends StatelessWidget {
                         onPressed: () {
                           if (formKey.currentState!.validate()) {
                             context.read<ForgetPasswordCubit>().doEvent(
-                                  ForgetPasswordResetPasswordEvent(
-                                    passwordController.text,
-                                    confirmPasswordController.text,
-                                  ),
-                                );
+                              ForgetPasswordResetPasswordEvent(
+                                passwordController.text,
+                                confirmPasswordController.text,
+                              ),
+                            );
                           }
                         },
                       );
