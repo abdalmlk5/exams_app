132154d113ae65a64a2f51fd6294f0e39d3cf91a
Abdelmalek <m7244985@gmail.com>
2026-04-27 19:16:45 +0300

    refactor(home): replace HomeBody with IndexedStack and remove AppBar for streamlined navigation

--- lib/features/home_screen/presentation/pages/home_screen.dart
+++ lib/features/home_screen/presentation/pages/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:exams_app/features/home_screen/presentation/cubit/home_cubit.dar
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../widgets/home_body.dart';
+import '../../../explore/presentation/pages/home_tab.dart';
 import '../widgets/home_bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,8 +16,16 @@ class HomeScreen extends StatelessWidget {
       child: BlocBuilder<HomeCubit, HomeState>(
         builder: (BuildContext context, HomeState state) {
           return Scaffold(
-            appBar: AppBar(),
-            body: HomeBody(selectedTab: state.selectedTab),
+            body: SafeArea(
+              child: IndexedStack(
+                index: state.selectedTab,
+                children: [
+                  HomeTab(),
+                  const Placeholder(color: Colors.red),
+                  const Placeholder(color: Colors.yellow),
+                ],
+              ),
+            ),
             bottomNavigationBar: HomeBottomNavBar(
               selectedTab: state.selectedTab,
             ),
